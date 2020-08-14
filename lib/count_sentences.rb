8bce931ad8e4edc769a816ac98cf4da76eb2ddaf
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-14 15:18:21 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,45 +2,26 @@ require 'pry'
 
 class String
   
-  attr_accessor :string, :count_sentences
+  attr_accessor :string
   
-  def initialize(string, count_sentences)
+  def initialize(string)
     @string = string
-    @count_sentences = count_sentences
   end
 
   def sentence?
-    if self.end_with?(".") == TRUE
-      TRUE
-    else 
-      false 
-    self.end_with?
+    if self.end_with?(".")
     end
   end 
 
   def question?
-    if self.end_with?("?") == true 
-      true
-    else
-      false 
+    if self.end_with?("?")
     end
   end 
 
   def exclamation?
-    if self.end_with?("!") == true
-      true
-    else
-      false 
+    if self.end_with?("!")
     end
   end
-
-  # def count_sentences
-  #   self.split(/\./).count #Has to begin with capital letter, not include ",", can include multiple punctuation at end. 
-    #   self.count
-    
-  # def count_sentences
-  # self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
-  # end
   
   def count_sentences
     self.split(' ').count { |word| word.sentence? || word.question? || word.exclamation? }

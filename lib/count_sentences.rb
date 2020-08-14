c597caab1cb40330536141e0a32ce8759ef1bdc0
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-14 14:43:54 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -30,6 +30,10 @@ class String
   def count_sentences
     self.split(/\./).count #Has to begin with capital letter, not include ",", can include multiple punctuation at end. 
     #   self.count
+    
+  def count_sentences
+   self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+  end
     end
   end 
 # end
